64b31b9ca5514129591465867689d1f0925b6aa1
Pieter Kitslaar <kitslaar@gmail.com>
2021-12-14 00:12:35 +0100

    266 characters

--- happy2022.py
+++ happy2022.py
@@ -1,5 +1,5 @@
 n,P={},lambda H,W,h=0,w=0:[(h+i//W,w+i%W)for i in range(W*H)]
-g=dict(zip(P(3,3,9,10),map(int,f'{191:b}')));G=lambda x:g.get(x,0)
+g=dict(zip(P(3,3,9,10),f'{191:b}'));G=lambda x:int(g.get(x,0))
 while 1:
- for p in P(20,40):y,x=p;V,S=G(p),sum(map(G,P(3,3,y-1,x-1)));n[p]=(V&(S in(3,4)))|((not V)&(S==3));print('.*'[n[p]],end='\n'*(x==39))
+ for p in P(20,40):y,x=p;V,S=G(p),sum(map(G,P(3,3,y-1,x-1)));n[p]=V&(2<S<5)|(V^1)&(S==3);print('.*'[n[p]],end='\n'*(x==39))
  g={**n}
