0d50243f1f3e0b68fd5f1b0b3265ad18a4b4ffc8
Pieter Kitslaar <pkitslaar@medisimaging.com>
2023-01-01 23:24:06 +0100

    Final version of happy2023.py

--- happy2023.py
+++ happy2023.py
@@ -1,43 +1,9 @@
-"""
-2222
-   *
-   *
-  **
-####
-*
-*
-**
-2222
-0000
-#  #
-#  #
-#  #
-#  #
-0000
-2222
-   *
-   *
-  ** 
-####
-*
-*
-**
-2222
-3333 
-   *
-  **
-   *    
-3333
-"""
-import time
-I=0
-#HVLlT
-#01234
-r,D,J,g=range,[4*"#",4*"*,","*,*,**"," *, *,**"," *,**, *"],32,{}
+r,D,J,g=range,[4*"#","2,22, 2"," 3,33, 3",3*"0  0,"],20,{}
 u=g.update
-for b in"0400203001002030":
- l=D[int(b)].split(',')[:4]
+for b in"020010030010":
+ x=int(b)
+ l=D[x].split(',')[:3]
  for y in r(J):
-  u({j:''for j in r(y+1)});u({y-j:v for j,v in zip(r(4),l)})
- for y in r(32):print(g.get(y,''))
+  u({j:''for j in r(y+1)});u({y-j:f"{' '*(x%3)}{v}"for j,v in zip(r(4),l)})
+  for y in r(20):print(g.get(y,''))
  J-=len(l)
